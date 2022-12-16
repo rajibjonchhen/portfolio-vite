daf776ca0582cc006f6eb9ae809d7271953a1874
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-12-16 11:17:22 +0000

    ready for deployment

--- data/myInfoData.js
+++ data/myInfoData.js
@@ -4,6 +4,6 @@ export default function myInfoData() {
     name:"Rita Vaidya",
     profileImg:"ProfileImg",
     title:"Account and administrative personnel",
-    description:"A full stack developer with Business and Communication background. I am looking forward to start a career as a developer.The hiring company can benefit from my education in Business and Communication, designing trainings and 3 years of managerial experience. I will definitely consider to make my projects and programmes cost efficient, strategically positioned, user friendly. ",
+    description:" I am an enthusiastic and motivated person with Master's in Economics and Business Administration with some years of experiences in accounting and administration. I enjoy working in a vibrant international environment and I am keen and very sincere about taking responsibility. Dynamic, thorough, and attentive to details are the attributes that some of my colleagues as well as my group mates from the university, described me as because of my uncompromising attention to detail and structure. ",
 }
 }
