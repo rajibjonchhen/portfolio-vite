887fbd3dd40ca6b68243308c3e12ecfb72282e76
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-12-16 14:08:42 +0000

    experience added

--- data/experienceData.js
+++ data/experienceData.js
@@ -8,10 +8,12 @@ export default function experienceData () {
         endDate:"03 / 2022",
         img:"https://res.cloudinary.com/dai5duzoj/image/upload/v1670432665/portfolio/natural-nails_khvvdj.png", 
         tasks :[
-            "Recruiting, training, motivating, mentoring floor and kitchen staff.",
-            "Ensuring compliance with policies and procedures and handling administration",
-            "Preparing financial reports, performance reports and strategies to improve sales and performance",
-        ],
+            "Hiring, training, motivating and coaching employees so as they provide attentive, efficient service to customers.",
+            "Preparing schedules and monitoring store activity.",
+            "Able to improve the rating from 4.6 to 4.9 with in a year.",
+           ],
+
+        
         contact:{
             name:"Hanna P Weiglin",
             email:"Naturalnailsdk@gmail.com"
@@ -40,9 +42,9 @@ export default function experienceData () {
         place:"Kathmandu, Nepal",
         startDate:"10 / 2013",
         endDate:"12 / 2015",
-        img:"https://res.cloudinary.com/dai5duzoj/image/upload/v1657118348/portfolio/sms_xme4mw.png", 
+        img:"https://res.cloudinary.com/dai5duzoj/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1671199184/portfolio/844DDB82-E798-4EF0-B646-295E0E43DFA4_ag7kqw_797b56.jpg", 
         tasks :[
-            "Keeping Company's Accounts",
+            "Keeping Company's Accounts, Invoicing, Internal Reporting",
             "Budget Analysis, Tax Computations",
             "Inventory Management",
         ],
